2054dbd0570d6435a6a8472a6ab9661d124ed866
jbrhm <134429827+jbrhm@users.noreply.github.com>
2023-11-27 09:24:30 -0500

    Working Random Website and Fixed All Access Bug

--- Main.cpp
+++ Main.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <unordered_map>
+#include <chrono>
 
 
 struct WebPage{
@@ -17,6 +18,9 @@ struct WebPage{
     //Vector of the WebPages this WebPage Links to
     std::vector<WebPage> links;
 
+    //Map of WebPage names to their index
+    std::unordered_map<std::string, bool> webPageMap;
+
     public:
     //Constructor for the webpage
     WebPage(std::string websiteName, std::string keyword){
@@ -26,6 +30,10 @@ struct WebPage{
 
     void setLinks(std::vector<WebPage> links){
         this->links = links;
+        
+        for(int i = 0; i < links.size(); i++){
+            webPageMap[links.at(i).getWebsiteName()] = true;
+        }
     }
 
     std::string getWebsiteName(){
@@ -43,12 +51,19 @@ struct WebPage{
         std::cin >> nextPage;
         return nextPage;
     }
+
+    bool isWebsiteAvailable(std::string name){
+        if(webPageMap.at(name) == true){
+            return true;
+        }
+        return false;
+    }
 };
 
 class SearchEngine{
     private:
     //vector of the webpages inside this search engine
-    std::vector<WebPage> pages;
+    std::vector<WebPage>* pages;
 
     //Current WebPage
     int websiteID;
@@ -58,21 +73,26 @@ class SearchEngine{
 
     public:
     //Constructor for the SearchEngine
-    SearchEngine(std::vector<WebPage> pages){
+    SearchEngine(std::vector<WebPage>* pages){
         this->pages = pages;
-        std::srand(std::time(nullptr));
-        websiteID = static_cast<int>((std::rand()/INT_MAX) * pages.size());
+        std::srand(std::time(NULL));
+        std::cout << (std::rand()/ (double) INT_MAX) << std::endl;
+        websiteID = static_cast<int>((std::rand()/ (double) RAND_MAX) * pages->size());
 
-        for(int i = 0; i < pages.size(); i++){
-            webPageMap[pages.at(i).getWebsiteName()] = i;
+        for(int i = 0; i < pages->size(); i++){
+            webPageMap[pages->at(i).getWebsiteName()] = i;
         }
     }
 
     void startExecution(){
         while(true){
-            std::string nextWebPage = pages.at(websiteID).loadWebPage();
+            std::string nextWebPage = pages->at(websiteID).loadWebPage();
             try{
-                websiteID = webPageMap.at(nextWebPage);
+                if(pages->at(websiteID).isWebsiteAvailable(nextWebPage)){
+                    websiteID = webPageMap.at(nextWebPage);
+                }else{
+                    throw std::out_of_range("");
+                }
             }catch(std::out_of_range){
                 std::cout << "WebPage not found. Try again!" << std::endl;
             }
@@ -81,10 +101,24 @@ class SearchEngine{
 };
 
 int main(){
-    WebPage google("Google", "Google");
-    WebPage bing("bing", "bing");
-    std::vector<WebPage> googleLinks{bing, bing};
+    WebPage google("Google", "Search Engine");
+    WebPage bing("Bing", "Search Engine");
+    WebPage duckDuckGo("DuckDuck", "Search Engine");
+
+    //Set Up Google's Links
+    std::vector<WebPage> googleLinks{bing, duckDuckGo};
     google.setLinks(googleLinks);
-    SearchEngine engine(googleLinks);
+
+    //Set Up Bing's Links
+    std::vector<WebPage> bingLinks{google, duckDuckGo};
+    bing.setLinks(bingLinks);
+
+    //Set Up Duck Duck Go's Links
+    std::vector<WebPage> duckDuckGoLinks{bing};
+    duckDuckGo.setLinks(duckDuckGoLinks);
+
+    //Set up the Engine Over the Entire Search Space
+    std::vector<WebPage> allLinks{google, bing, duckDuckGo};
+    SearchEngine engine(&allLinks);
     engine.startExecution();
 }
